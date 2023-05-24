1186d7144e79d57c1588079a7b6ae721bf35f612
Aboogie <albrown.ctr@kr.af.mil>
2023-05-24 13:13:40 +0300

    Adding middleware

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -3,13 +3,23 @@ package main
 import (
 	"html/template"
 	"path/filepath"
+	"time"
 
 	"snippetbox.alfredbrowniii.io/internal/models"
 )
 
 type templateData struct {
-	Snippet  *models.Snippet
-	Snippets []*models.Snippet
+	CurrentYear int
+	Snippet     *models.Snippet
+	Snippets    []*models.Snippet
+}
+
+func humanDate(t time.Time) string {
+	return t.Format("02 Jan 2066 at 15:04")
+}
+
+var functions = template.FuncMap{
+	"humanDate": humanDate,
 }
 
 func newTemplateCache() (map[string]*template.Template, error) {
@@ -28,22 +38,22 @@ func newTemplateCache() (map[string]*template.Template, error) {
 		//Extract file name and assign to variable
 		name := filepath.Base(page)
 
-		ts, err := template.ParseFiles("./ui/html/base.tmpl")
+		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.tmpl")
 		if err != nil {
 			return nil, err
 		}
 
-		ts, err := ts.ParseGlob("./ui/html/partials/*.tmpl")
+		tsa, err := ts.ParseGlob("./ui/html/partials/*.tmpl")
 		if err != nil {
 			return nil, err
 		}
 
-		ts, err := ts.ParseFiles(page)
+		tsb, err := tsa.ParseFiles(page)
 		if err != nil {
 			return nil, err
 		}
 
-		cache[name] = ts
+		cache[name] = tsb
 	}
 
 	return cache, nil
