3448466738245e497ae9f1f170a47afdfce4b914
Aboogie <albrown.ctr@kr.af.mil>
2023-05-28 23:03:29 +0300

    Problem with login page and create page added protected routes /middleware added for authenticateduser

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -17,7 +17,7 @@ func (app *application) routes() http.Handler {
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
 
-	dynamic := alice.New(app.sessionManager.LoadAndSave)
+	dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf, app.authenticate)
 
 	// .Handle("/static/", http.StripPrefix("/static", fileServer))
 	router.Handler(http.MethodGet, "/", dynamic.ThenFunc(app.home))
@@ -29,9 +29,9 @@ func (app *application) routes() http.Handler {
 
 	protected := dynamic.Append(app.requireAuthentication)
 
-	router.Handler(http.MethodPost, "/user/logout", protected.ThenFunc(app.userLogoutPost))
 	router.Handler(http.MethodGet, "/snippet/create", protected.ThenFunc(app.snippetCreate))
 	router.Handler(http.MethodPost, "/snippet/create", protected.ThenFunc(app.snippetCreatePost))
+	router.Handler(http.MethodPost, "/user/logout", protected.ThenFunc(app.userLogoutPost))
 	// Create a middleware chain
 
 	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
