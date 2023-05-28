a6388cda53ed89ea4e1c71bd902423edec6915a4
Aboogie <albrown.ctr@kr.af.mil>
2023-05-28 21:47:17 +0300

    Problem with login page and create page added protected routes

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -261,5 +261,16 @@ func (app *application) userLoginPost(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) userLogoutPost(w http.ResponseWriter, r *http.Request) {
+	err := app.sessionManager.RenewToken(r.Context())
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	app.sessionManager.Remove(r.Context(), "authenticatedUserID")
+
+	app.sessionManager.Put(r.Context(), "flash", "you've been logged out")
+
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 	fmt.Fprintln(w, "Logout the user...")
 }
